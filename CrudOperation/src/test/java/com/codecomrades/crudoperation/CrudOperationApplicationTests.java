f100da56d6f2df9b26f94eb7dcfcb922ed01275f
somjena <jenasoumitrapvt@gmail.com>
2025-04-29 01:08:20 +0530

    Initializing The Project of Simple Crud Application

---
+++ CrudOperation/src/test/java/com/codecomrades/crudoperation/CrudOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecomrades.crudoperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
