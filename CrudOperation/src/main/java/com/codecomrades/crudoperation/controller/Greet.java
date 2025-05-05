1cc82e910b3e519dc455aeba809090526c6613f9
somjena <jenasoumitrapvt@gmail.com>
2025-05-05 21:09:43 +0530

    Controller

--- CrudOperation/src/main/java/com/codecomrades/crudoperation/controller/Greet.java
+++ CrudOperation/src/main/java/com/codecomrades/crudoperation/controller/Greet.java
@@ -1,12 +1,21 @@
 package com.codecomrades.crudoperation.controller;
 
+import com.codecomrades.crudoperation.entity.Student;
+import com.codecomrades.crudoperation.service.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Greet {
-    @GetMapping("/welcome")
-    public String hello(){
-        return "Hey Welcome to our Application";
+    @Autowired
+    Service service;
+    @PostMapping("/save")
+    public String register(@RequestBody Student student){
+        service.save(student);
+        return "Data Saved Successfully";
+
     }
 }
