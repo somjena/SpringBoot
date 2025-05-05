7a8df4c222e4a5ea39f89d198abda2ecc9957fec
somjena <jenasoumitrapvt@gmail.com>
2025-05-05 21:09:50 +0530

    Student Class

--- CrudOperation/src/main/java/com/codecomrades/crudoperation/entity/Student.java
+++ CrudOperation/src/main/java/com/codecomrades/crudoperation/entity/Student.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import java.util.List;
+
 @Entity
 @Table(name = "StudentData")
 public class Student {
@@ -13,5 +15,28 @@ public class Student {
     private long id;
     private String name;
     private int age;
-    
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
