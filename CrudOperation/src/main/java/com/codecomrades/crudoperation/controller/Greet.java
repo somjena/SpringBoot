0055ff5ea5fd81d7dbcf933b58477d343bf4b958
somjena <jenasoumitrapvt@gmail.com>
2025-05-07 21:21:52 +0530

    Fetch All The Student

--- CrudOperation/src/main/java/com/codecomrades/crudoperation/controller/Greet.java
+++ CrudOperation/src/main/java/com/codecomrades/crudoperation/controller/Greet.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class Greet {
     @Autowired
@@ -18,4 +20,9 @@ public class Greet {
         return "Data Saved Successfully";
 
     }
+    @GetMapping("/getall")
+    public List<Student>get(){
+       return service.getall();
+    }
 }
+
