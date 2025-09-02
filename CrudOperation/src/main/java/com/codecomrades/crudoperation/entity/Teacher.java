41466e90037338c9d9807f29ecf997de9f664a69
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-09-02 06:30:52 +0530

    Add Teacher entity and configure PostgreSQL
    Introduced the Teacher entity class for CRUD operations. Updated application properties to use PostgreSQL instead of MySQL and added JPA/Hibernate settings. Initialized Distribution_ManageMent project with .gitattributes, .gitignore, Maven wrapper properties, and mvnw script.

---
+++ CrudOperation/src/main/java/com/codecomrades/crudoperation/entity/Teacher.java
@@ -0,0 +1,27 @@
+package com.codecomrades.crudoperation.entity;
+
+public class Teacher {
+    private long id;
+    private String name;
+
+    public Teacher(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
